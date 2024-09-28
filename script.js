117201f8b75fc344702e2f9d18c52cceb0cce66f
Bazif Abdullah <144853581+bazifabdullah@users.noreply.github.com>
2024-09-28 19:48:54 +0000

    Add error messages dynamically to each input

--- script.js
+++ script.js
@@ -14,6 +14,12 @@ const inputElements = {
     cardCVCInput: document.getElementById("CVCInput"),
 };
 
+const submitButton = document.getElementById("submitButton");
+const continueButton = document.getElementById("continueButton");
+const formSection = document.getElementById('formSection');
+const form = document.getElementById('form');
+const completeState = document.getElementById('completeState');
+
 function displayValue(e, elementKey) {
     cardElements[elementKey].innerText = format(e.target.value);
 }
@@ -33,3 +39,65 @@ const mapping = {
 for (const inputKey in mapping) {
     inputElements[inputKey].addEventListener('keyup', (e) => {displayValue(e, mapping[inputKey])})
 }
+
+function handleSubmit(e) {
+    e.preventDefault();
+    let hasError = false;
+
+    for(const inputKey in inputElements) {
+        const inputElement = inputElements[inputKey];
+
+        const existingErrorMessage = inputElement.parentElement.querySelector('.error_msg');
+        if (existingErrorMessage) {
+            existingErrorMessage.remove();
+        }
+
+        if (!inputElement.value) {
+            const errorMessage = document.createElement('p');
+            errorMessage.innerText = "This field can't be blank";
+            errorMessage.classList.add("error_msg");
+            inputElement.parentElement.appendChild(errorMessage);
+            inputElement.classList.add("error_border");
+            hasError = true;
+        } else {
+            inputElement.classList.remove("error_border");
+        }
+    }
+
+    if (!hasError) {
+        formSection.classList.add('hidden');
+        completeState.classList.remove('hidden');
+    }
+}
+
+function resetfunction() {
+    formSection.classList.remove('hidden');
+    completeState.classList.add('hidden');
+    form.reset();
+}
+
+const cardNumberInput = inputElements['cardNumberInput'];
+cardNumberInput.addEventListener('keyup', validateCardNumber);
+
+function validateCardNumber() {
+    const inputElement = cardNumberInput;
+    const existingErrorMessage = inputElement.parentElement.querySelector('.error_msg');
+    if (existingErrorMessage) {
+        existingErrorMessage.remove();
+    }
+
+    if (/[a-zA-Z]/.test(inputElement.value)) {
+        const errorMessage = document.createElement('p');
+        errorMessage.innerText = "Wrong Input, Numbers Only";
+        errorMessage.classList.add("error_msg");
+        inputElement.parentElement.appendChild(errorMessage);
+        inputElement.classList.add("error_border");
+        hasError = true;
+    } else {
+        inputElement.classList.remove('error_border');
+    }
+}
+
+
+submitButton.addEventListener('click', handleSubmit);
+continueButton.addEventListener('click', resetfunction);
